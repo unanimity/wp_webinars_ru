e6bba1b8ec87b5eb180c5ac16dd8c050890e5997
swoftbook.ru <unanimity0@gmail.ru>
2018-06-05 21:19:14 +0300

    mk api request - ok

--- admin/class-webinars-ru-admin.php
+++ admin/class-webinars-ru-admin.php
@@ -100,6 +100,8 @@ class Webinars_RU_Admin {
 
 	}
 
+
+
 }
 
 
@@ -108,14 +110,45 @@ add_action('admin_menu', 'add_webinars_ru_plugin_page');
 function add_webinars_ru_plugin_page(){
 	add_options_page( 'Настройки Webinars_ru', 'Webinars Ru', 'manage_options', 'webinars_ru_slug', 'webinars_ru_options_page_output' );
 }
+
+
 function webinars_ru_options_page_output(){
      ?>
 
 
-    <form action="" id="ajax_form" style=" margin:14px;">
-            <label for="visitor_name"><h3>Запустить автоматическую простановку ссылок (полный цикл)?</h3></label>
-            <button type="button" name="autolink_submit_form" id="autolink_submit_form"  class="btn btn-dark">Запустить</button>
-    </form>
+    <div class="wrap">
+		<h2><?php echo get_admin_page_title() ?></h2>
+
+		<form action="options.php" method="POST">
+			<?php
+				settings_fields( 'webinar_ru_options' );     // скрытые защитные поля
+				do_settings_sections( 'webinar_ru_settings' ); // секции с настройками (опциями). У нас она всего одна 'section_id'
+				submit_button();
+				
+				
+				
+    	$val = get_option('api_key');
+    	$val = $val ? $val['input'] : null;
+	
+		 $request = wp_remote_get( 'https://userapi.webinar.ru/v3/organization/members',
+             array( 'timeout' => 10,
+            'headers' => array( 'X-Auth-Token' => $val) 
+             ) );
+             
+           //  echo $val;
+            if( is_wp_error( $request ) ) {
+            	return false; // Bail early
+            }
+            $body = wp_remote_retrieve_body( $request );
+            $data = json_decode( $body );
+            if( ! empty( $data ) ) {
+            	
+            	echo ' <h3>Admin Email:  '.$data[0]->email.'</h3> <img src="'.$data[0]->photo->thumbnails->{'640x1920'}.'"  >';
+              
+            }
+			?>
+		</form>
+	</div>
     
 
 
@@ -125,5 +158,55 @@ function webinars_ru_options_page_output(){
     <!-- The result of the search will be rendered inside this div -->
  
     <?php
- 
 }
+
+add_action('admin_init', 'webinar_ru_settings');
+function webinar_ru_settings(){echo "-1";
+	// параметры: $option_group, $option_name, $sanitize_callback
+	register_setting( 'webinar_ru_options', 'api_key', 'sanitize_callback' );
+
+	// параметры: $id, $title, $callback, $page
+	add_settings_section( 'webinar_ru_section_id', 'Основные настройки:', '', 'webinar_ru_settings' ); 
+
+	// параметры: $id, $title, $callback, $page, $section, $args
+	add_settings_field('api_key_id', 'Ключь API ', 'webinar_ru_api_key', 'webinar_ru_settings', 'webinar_ru_section_id' );
+//	add_settings_field('primer_field2', 'Другая опция', 'fill_primer_field2', 'webinar_ru_settings', 'webinar_ru_section_id' );
+
+}
+
+## Заполняем опцию 1
+function webinar_ru_api_key(){
+	$val = get_option('api_key');
+	$val = $val ? $val['input'] : null;
+ 
+	?>
+	<input type="text" name="api_key[input]" value="<?php echo esc_attr( $val ) ?>" />
+	<?php
+}
+
+## Заполняем опцию 2
+function fill_primer_field2(){
+	$val = get_option('option_name');
+	$val = $val ? $val['checkbox'] : null;
+	?>
+	<label><input type="checkbox" name="option_name[checkbox]" value="1" <?php checked( 1, $val ) ?> /> отметить</label>
+	<?php
+}
+
+## Очистка данных
+function sanitize_callback( $options ){ 
+	// очищаем
+	foreach( $options as $name => & $val ){
+		if( $name == 'input' )
+			$val = strip_tags( $val );
+
+	 
+	}
+ 
+	//die(print_r( $options )); // Array ( [input] => aaaa [checkbox] => 1 )
+
+	return $options;
+}
+
+
+
